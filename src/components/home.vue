671147c6feb8769e53c098b27d553e0ca2ff13cc
qi <qi0801@qq.com>
2019-01-01 14:16:50 +0800

    首页的头部和侧边栏完成

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,13 +1,128 @@
 <template>
-    <div>home</div>
+  <el-container class="container">
+    <el-header class="header">
+      <el-row>
+        <el-col :span="3">
+          <div class="grid-content bg-purple">
+            <img src="@/assets/logo.png" alt="哇,图片飞了">
+          </div>
+        </el-col>
+        <el-col :span="20" class="middle">
+          <h2>电 商 管 理 平 台</h2>
+        </el-col>
+        <el-col :span="1" class="loginout">
+          <a href>退出</a>
+        </el-col>
+      </el-row>
+    </el-header>
+    <el-container>
+      <el-aside width="200px" class="aside">
+        <el-menu unique-opened default-active="2">
+          <!-- 1 -->
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+
+            <el-menu-item index="1-3">
+              <i class="el-icon-menu"></i>
+              用户列表
+            </el-menu-item>
+          </el-submenu>
+          <!-- 2 -->
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>权限管理</span>
+            </template>
+
+            <el-menu-item index="1-3">
+              <i class="el-icon-menu"></i>
+              角色列表
+            </el-menu-item>
+            <el-menu-item index="1-4">
+              <i class="el-icon-menu"></i>
+              权限列表
+            </el-menu-item>
+          </el-submenu>
+          <!-- 3 -->
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>商品管理</span>
+            </template>
+
+            <el-menu-item index="1-3">
+              <i class="el-icon-menu"></i>
+              商品列表
+            </el-menu-item>
+            <el-menu-item index="1-3">
+              <i class="el-icon-menu"></i>
+              分类参数
+            </el-menu-item>
+            <el-menu-item index="1-3">
+              <i class="el-icon-menu"></i>
+              商品分类
+            </el-menu-item>
+          </el-submenu>
+          <!-- 4 -->
+          <el-submenu index="4">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>订单管理</span>
+            </template>
+
+            <el-menu-item index="1-3">
+              <i class="el-icon-menu"></i>
+              订单列表
+            </el-menu-item>
+          </el-submenu>
+          <!-- 5 -->
+          <el-submenu index="5">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>数据统计</span>
+            </template>
+
+            <el-menu-item index="1-3">
+              <i class="el-icon-menu"></i>
+              数据报表
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main class="main">Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
+.container {
+  height: 100%;
+  /* background-color: red; */
+}
+.container .header {
+  background-color: #b3c0d1;
+}
+.container .aside {
+  /* background-color: blue; */
+}
+.container .main {
+  /* background-color: pink; */
+}
+.middle {
+  text-align: center;
+  line-height: 100%;
+  color: #fff;
+}
+.loginout a {
+  line-height: 4;
+  text-decoration: none;
+  color: blue;
+}
 </style>
--- src/components/login.vue
+++ src/components/login.vue
@@ -1,16 +1,16 @@
 <template>
-<div id='warp'>
-  <el-form class='form' label-position="top" label-width="80px" :model="formData">
-    <h2>用户登录</h2>
-    <el-form-item label="用户名">
-      <el-input v-model="formData.username"></el-input>
-    </el-form-item>
-    <el-form-item label="密  码">
-      <el-input v-model="formData.password"></el-input>
-    </el-form-item>
-    <el-button class='btn' type="primary" @click='handleLogin()'>登陆</el-button>
-  </el-form>
-</div>
+  <div id="warp">
+    <el-form class="form" label-position="top" label-width="80px" :model="formData">
+      <h2>用户登录</h2>
+      <el-form-item label="用户名">
+        <el-input v-model="formData.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密  码">
+        <el-input v-model="formData.password"></el-input>
+      </el-form-item>
+      <el-button class="btn" type="primary" @click="handleLogin()">登陆</el-button>
+    </el-form>
+  </div>
 </template>
 
 <script>
@@ -25,21 +25,24 @@ export default {
   },
   methods: {
     // 登录请求
-    handleLogin () {
-      this.$http
-        .post('login', this.formData)
-        .then((res) => {
-          console.log(res)
-          const {data:{meta: {msg, status}}, data} = res
-          if (status === 200) {
-            this.$message(msg)
-            this.$router.push({
-              name: 'home'
-            })
-          } else {
-            this.$message.warning(msg)
-          }
+    async handleLogin () {
+      const res = await this.$http.post('login', this.formData)
+      console.log(res)
+      const {
+        data: {
+          meta: { msg, status },
+          data
+        }
+      } = res
+      if (status === 200) {
+        localStorage.setItem('token', data.token)
+        this.$message(msg)
+        this.$router.push({
+          name: 'home'
         })
+      } else {
+        this.$message.warning(msg)
+      }
     }
   }
 }
@@ -52,7 +55,6 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
-
 }
 #warp .form {
   background-color: #fff;
